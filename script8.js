8123f7a532075871f3496e212cf7bf137cc0fd09
Alex-Pobeda <34069629+Alex-Pobeda@users.noreply.github.com>
2018-01-21 23:24:42 +0200

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,33 @@
+const playSound = note => {
+  const audio = document.querySelector(`audio[data-note=${note}]`);
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
+
+let pressedKey = function(e){
+	let keyName = e.key;
+	let input = document.querySelector("input:checked");
+	
+	for(var i = 0; i < buttons.length; i++){
+		if((buttons[i].textContent == keyName) && (keys.indexOf(keyName) != -1) && input) {
+			let note = buttons[i].getAttribute("data-note");
+			buttons[i].classList.add("keyboard__btn--active");
+			playSound(note);
+		} else if ((buttons[i].textContent == "space") && (keys.indexOf(keyName) == -1) && input){
+			let note = buttons[i].getAttribute("data-note");
+			buttons[i].classList.add("keyboard__btn--active");
+			playSound(note);
+		}
+	}
+};
+
+let upedKey = function(e){
+	for(var i = 0; i < buttons.length; i++){
+		buttons[i].classList.remove("keyboard__btn--active");
+	}
+};
+window.addEventListener("keydown", pressedKey);
+window.addEventListener("keyup", upedKey);
